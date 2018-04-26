296ba5284a110e1ec8b6cc3358362012ffaf2481
mark meyers <kilfour@gmail.com>
2018-04-26 20:59:25 +0200

    composition v0.2

--- QuickXmlWrite.Tests/ComposingWriters.cs
+++ QuickXmlWrite.Tests/ComposingWriters.cs
@@ -5,7 +5,7 @@ namespace QuickXmlWrite.Tests
     public class ComposingWriters
     {
         [Fact]
-        public void Example()
+        public void OneLevel()
         {
             var intWriter = XmlWrite<int>.Tag("int").Content(x => x.ToString());
 
@@ -15,6 +15,26 @@ namespace QuickXmlWrite.Tests
                 select root;
 
             var expected = "<root><int>42</int></root>";
+            var actual = writer.Write(42);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TwoLevels()
+        {
+            var intWriter = XmlWrite<int>.Tag("int").Content(x => x.ToString());
+
+            var subWriter =
+                from root in XmlWrite<int>.Tag("sub")
+                from sub in intWriter
+                select root;
+
+            var writer =
+                from root in XmlWrite<int>.Tag("root")
+                from sub in subWriter
+                select root;
+
+            var expected = "<root><sub><int>42</int></sub></root>";
             var actual = writer.Write(42);
             Assert.Equal(expected, actual);
         }
