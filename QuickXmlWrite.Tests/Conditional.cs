79b39adbab4e5442d9a340c3312da4b300883ea7
mark meyers <kilfour@gmail.com>
2018-08-07 10:54:50 +0200

    test fix

--- QuickXmlWrite.Tests/Conditional.cs
+++ QuickXmlWrite.Tests/Conditional.cs
@@ -1,33 +1,25 @@
-﻿using Xunit;
+﻿using QuickXmlWrite.UnderTheHood;
+using Xunit;
 
 namespace QuickXmlWrite.Tests
 {
     public class Conditional
     {
+        private static readonly XmlWriter<XmlWriterNode<int>> Writer = 
+            from root in XmlWrite.For<int>().Tag("root")
+            from sub in root.Tag("int").Content(x => x.ToString()).If(x => x == 42)
+            select root;
+
         [Fact]
         public void IsTrue()
         {
-            var intWriter = XmlWrite.For<int>().Tag("int").Content(x => x.ToString());
-            var writer =
-                from root in XmlWrite.For<int>().Tag("root")
-                from sub in root.Tag("int").Content(x => x.ToString()).If(x => x == 42)
-                select root;
-            var expected = "<root><int>42</int></root>";
-            var actual = writer.Write(42);
-            Assert.Equal(expected, actual);
+            Assert.Equal("<root><int>42</int></root>", Writer.Write(42));
         }
 
         [Fact]
         public void IsFalse()
         {
-            var intWriter = XmlWrite.For<int>().Tag("int").Content(x => x.ToString());
-            var writer =
-                from root in XmlWrite.For<int>().Tag("root")
-                from sub in root.Tag("int").Content(x => x.ToString()).If(x => x == 42)
-                select root;
-            var expected = "<root><int>42</int></root>";
-            var actual = writer.Write(42);
-            Assert.Equal(expected, actual);
+            Assert.Equal("<root/>", Writer.Write(666));
         }
     }
 }
