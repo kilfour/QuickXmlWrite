e3084a78f10ab7aa40dc449559b4ad26d59262fb
mark meyers <kilfour@gmail.com>
2018-09-05 22:38:38 +0200

    apply is needed

---
+++ QuickXmlWrite.Tests/Apply.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace QuickXmlWrite.Tests
+{
+    public class Apply
+    {
+        [Fact]
+        public void Spike()
+        {
+            var intWriter = XmlWrite.For<int>().Tag("int").Content(x => x.ToString());
+
+            var writer =
+                from root in XmlWrite.For<int>().Tag("root")
+                from sub in root.Apply(intWriter)
+                select root;
+
+            var expected = "<root><int>42</int></root>";
+            var actual = writer.Write(42);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ QuickXmlWrite/Apply.cs
@@ -0,0 +1,35 @@
+﻿
+
+using QuickXmlWrite.UnderTheHood;
+
+namespace QuickXmlWrite
+{
+    public static partial class XmlWriteExtensions
+    {
+        public static XmlWriter<XmlWriterNode<TInput>> Apply<TInput>(this XmlWriterNode<TInput> writerNode, XmlWriter<XmlWriterNode<TInput>> innerWriter)
+        {
+            return state =>
+            {
+                var oldNode = state.Current;
+                innerWriter(state);
+                state.Current = oldNode;
+                return Result<TInput>.WriterNodeResultFromState(state);
+            };
+        }
+
+        public static XmlWriter<XmlWriterNode<TInput>> Apply<TInput>(this XmlWriterNode<TInput> writerNode, XmlWriterAction<TInput> innerWriterAction)
+        {
+            return state =>
+            {
+                var oldInput = state.CurrentInput;
+                var oldNode = state.Current;
+                IResult<XmlWriterNode<TInput>> InnerWriter(State innerState) => new Result<XmlWriterNode<TInput>>(new XmlWriterNode<TInput>(innerState.Current), innerState);
+                var appliedWriter = innerWriterAction(InnerWriter);
+                appliedWriter(state);
+                state.Current = oldNode;
+                state.CurrentInput = oldInput;
+                return Result<TInput>.WriterNodeResultFromState(state);
+            };
+        }
+    }
+}
