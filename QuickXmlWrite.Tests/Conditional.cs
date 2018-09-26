c8d2ec518c92a347bcf979346f98e67fcd02fb72
mark meyers <kilfour@gmail.com>
2018-09-27 00:44:37 +0200

    Conditional test

--- QuickXmlWrite.Tests/Conditional.cs
+++ QuickXmlWrite.Tests/Conditional.cs
@@ -21,5 +21,38 @@ namespace QuickXmlWrite.Tests
         {
             Assert.Equal("<root/>", Writer.Write(666));
         }
+
+        [Fact]
+        public void MultipleAttributesFirstOnly()
+        {
+            var writer = from root in XmlWrite.For<int>().Tag("root")
+                from sub1 in root.Attribute("attr1", "yep").If(x => x == 42)
+                from sub2 in root.Attribute("attr2", "again").If(x => x != 42)
+                select root;
+
+            Assert.Equal("<root attr1=\"yep\"/>", writer.Write(42));
+        }
+
+        [Fact]
+        public void MultipleAttributesSecondOnly()
+        {
+            var writer = from root in XmlWrite.For<int>().Tag("root")
+                from sub1 in root.Attribute("attr1", "yep").If(x => x != 42)
+                from sub2 in root.Attribute("attr2", "again").If(x => x == 42)
+                select root;
+
+            Assert.Equal("<root attr2=\"again\"/>", writer.Write(42));
+        }
+
+        [Fact]
+        public void MultipleAttributesBoth()
+        {
+            var writer = from root in XmlWrite.For<int>().Tag("root")
+                from sub1 in root.Attribute("attr1", "yep").If(x => x == 42)
+                from sub2 in root.Attribute("attr2", "again").If(x => x == 42)
+                select root;
+
+            Assert.Equal("<root attr1=\"yep\" attr2=\"again\"/>", writer.Write(42));
+        }
     }
 }
